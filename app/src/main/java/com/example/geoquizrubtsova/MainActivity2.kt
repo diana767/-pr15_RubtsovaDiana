be4cdb6ff523dd4ad56fea0ed21b8f874fdc3816
ural-ECTS <ural@ects.ru>
2024-05-23 15:01:40 +0500

    практическая 15-16

---
+++ app/src/main/java/com/example/geoquizrubtsova/MainActivity2.kt
@@ -0,0 +1,69 @@
+package com.example.geoquizrubtsova
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.view.Gravity
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+
+
+class MainActivity2 : AppCompatActivity() {
+
+    private lateinit var trueButton: Button
+    private lateinit var falseButton: Button
+    private lateinit var textView: TextView
+    data class cls( val quustion: String, val answer: Boolean)
+    private var index=0
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        trueButton = findViewById(R.id.true_button)
+        falseButton = findViewById(R.id.false_button)
+        textView = findViewById(R.id.text_view)
+        trueButton.setOnClickListener {
+            checkAnswer(true)
+        }
+        falseButton.setOnClickListener {
+            checkAnswer(false)
+        }
+        initializeQuestionsAndAnswers()
+        setQuestion()
+    }
+    private lateinit var list: List<cls>
+  private fun initializeQuestionsAndAnswers(){
+      list= listOf(
+    cls(getString(R.string.string_text),true),
+          cls(getString(R.string.string_text2),true),
+    cls(getString(R.string.string_text3),true),
+          cls(getString(R.string.string_text4),false),
+    cls(getString(R.string.string_text5),false),
+          )
+  }
+
+private fun setQuestion(){
+    val curentQuestion=list[index]
+    textView.text=curentQuestion.quustion
+}
+private fun checkAnswer(userAnswer:Boolean){
+    val curentQuestion =list[index]
+    if (userAnswer==curentQuestion.answer){
+        val toast=Toast.makeText(this,"Верно!",Toast.LENGTH_SHORT)
+        toast.setGravity(Gravity.TOP,0,0)
+        toast.show()
+    }else
+    {        val toast=Toast.makeText(this,"Не верно!",Toast.LENGTH_SHORT)
+        toast.setGravity(Gravity.TOP,0,0)
+        toast.show()
+    }
+    index++
+    if(index<list.size){
+        setQuestion()
+    }else
+    {        Toast.makeText(this,R.string.end,Toast.LENGTH_SHORT).show()
+        application.onTerminate()    }
+}
+
+
+}
